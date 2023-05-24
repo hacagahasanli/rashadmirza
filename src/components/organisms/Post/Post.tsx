8cd3f8f6dda3032d510edfbc738d104f18892295
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-05-24 16:23:11 +0400

    scrolltotopheader little change

--- src/components/organisms/Post/Post.tsx
+++ src/components/organisms/Post/Post.tsx
@@ -1,7 +1,9 @@
 
 import { Footer, Header, PostDetails } from "components";
+import { useScrollToTop } from "hooks";
 
 export const Post = () => {
+    useScrollToTop()
     return (
         <>
             <Header />
